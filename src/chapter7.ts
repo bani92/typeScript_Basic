e2c51bc4a0531d9066fc63b5b846e0cd8395ae2d
bani <dalllo9851@gmail.com>
2025-04-08 21:59:45 +0900

    [2일차] ENUM, Any, Void, Never

--- src/chapter7.ts
+++ src/chapter7.ts
@@ -11,3 +11,13 @@ function fun2(): void {
 
 // never
 // 불가능한 타입
+function func3(): never {
+  throw new Error();
+}
+
+// 타입호환성
+let num1: number = 10;
+let num2: 10 = 10;
+
+num1 = num2;
+// num2 = num1; 불가능(리터럴타입에는 넣을 수 없음, 다운캐스팅)
