694ba3333a1d33fb1966c558e3e98e06089f4248
bani <dalllo9851@gmail.com>
2025-04-10 21:59:01 +0900

    [3일차] 기본타입 살펴보기

--- src/chapter3.ts
+++ src/chapter3.ts
@@ -1,14 +1,49 @@
-let user: {
-  id?: number;
-  name: string;
-} = {
-  id: 1,
-  name: "bani",
-};
-// 객체 리터럴타입
+// Unknown 타입
 
-let config: {
-  readonly apiKey: string;
-} = {
-  apiKey: "MY API KEY",
-};
+function unkownExam() {
+  let a: unknown = 1;
+  let b: unknown = "hello";
+  let c: unknown = true;
+
+  let unknownVar: unknown;
+
+  // let num: number = unknownVar; // 다운캐스팅 불가능
+}
+
+/**
+ * Never 타입
+ */
+
+function neverExam() {
+  function neverFunc(): never {
+    while (true) {}
+  }
+
+  let num: number = neverFunc(); // never -> number 업캐스팅 가능
+
+  // let never1: never = 10; // number -> never 다운캐스팅 불가능
+}
+
+/**
+ * Void 타입
+ */
+function voidExam() {
+  function voidFunc(): void {
+    console.log("hi");
+  }
+
+  let voidVar: void = undefined; // undefined -> void 업캐스팅 가능
+}
+
+/**
+ * any 타입
+ */
+
+function anyExam() {
+  let unknownVar: unknown;
+  let anyVar: any;
+  let neverVar: never;
+
+  anyVar = unknownVar; // unknown -> any 다운캐스팅 가능
+  // neverVar = anyVar; // any -> never 다운캐스팅 불가능
+}
